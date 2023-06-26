8a5678f6e158df31ac87c02f83ec4cbe7f5f3225
Venkat <venka7m@gmail.com>
2023-06-26 11:35:41 +0530

    c

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+
 function App() {
-	return (
-		<div className="App">
-			<Outlet />
-		</div>
-	);
+	return <div className="App">Hello</div>;
 }
 
 export default App;
